1f2423b6fa0bd76d29e646947587c5d1b02b4ad1
sinhoyes <37301041+sinhoyes@users.noreply.github.com>
2018-05-16 18:15:20 +0900

    18.5.16 타일준비
    18.5.16 타일준비

---
+++ H5Client/Assets/Script/H5Object/H5TileBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TILE_TYPE
+{
+    TILE_TYPE_NONE,
+    TILE_TYPE_NORMAL
+}
+public class H5TileBase : H5ObjectBase
+{
+
+    public virtual void InitObject()
+    {
+        var loadMaterial = Resources.Load("Material/Tile") as Material;
+    }
+}
